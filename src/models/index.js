3a89abcc04f7f0ca347081958b2c148fa3aae9ad
Dalin  Oluoch <mcdalinoluoch@gmail.com>
2019-04-18 17:37:47 +0300

    [Chore] Setup environment variables

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
+import ENV from '../config';
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
+const sequelize = new Sequelize(ENV.DB_NAME, ENV.DB_USER, ENV.DB_PASS, {
+  host: ENV.DB_HOST,
+  dialect: ENV.DB_DIALECT,
 });
 
 export default sequelize;
