7a3b3a36690e5de8a1c75c0c6a7aa9c6de044ab8
Dalin  Oluoch <mcdalinoluoch@gmail.com>
2019-04-21 19:56:53 +0300

    [Chore] Split typeDefs and Resolvers

--- src/models/user.js
+++ src/models/user.js
@@ -1,15 +1,32 @@
-const UserModel = (sequelize, type) => {
-  return sequelize.define('users', {
-    id: {
-      type: type.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+const User = (sequelize, DataTypes) => {
+  return sequelize.define(
+    'users',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    firstName: type.STRING,
-    lastName: type.STRING,
-    email: type.STRING,
-    password: type.STRING,
-  });
+    {
+      timestamps: false,
+    },
+  );
 };
 
-export default UserModel;
+export default User;
--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,13 +1,4 @@
-import { gql } from 'apollo-server';
+import root from './root';
+import user from './user';
 
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`;
-
-export default typeDefs;
+export default [root, user];
